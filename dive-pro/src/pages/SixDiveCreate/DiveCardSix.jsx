4a8b1ba12a0c4401ee76bc281bb3ab204dbe0544
DESKTOP-EOVF5KP\ctrob <ctrobinson2232@gmail.com>
2022-03-27 16:52:29 -0600

    commit 3-27

--- dive-pro/src/pages/SixDiveCreate/DiveCardSix.jsx
+++ dive-pro/src/pages/SixDiveCreate/DiveCardSix.jsx
@@ -42,6 +42,34 @@ const DiveCardSix = (props) => {
         else
             return ('')
     })
+    const position = pos.map(d => {
+        if (d.position === 'Straight')
+            return ('Str.')
+        else if (d.position === 'Pike')
+            return ('Pike')
+        else if (d.position === 'Tuck')
+            return ('Tuck')
+        else if (d.position === 'Free')
+            return ('Free')
+        else
+            return ('')
+    })
+
+    //DDs
+    const optdifficulty = (parseFloat(d2.difficulty) + parseFloat(d3.difficulty) + parseFloat(d4.difficulty) + parseFloat(d5.difficulty) + parseFloat(d6.difficulty))
+    function financial(x) {
+        return x.toFixed(1)
+    }
+    const finalDD = financial(optdifficulty)
+    var volDifficulty = ''
+    const dv = parseFloat(d1.difficulty)
+    if (dv > 1.8)
+        volDifficulty = 1.8
+    else
+        volDifficulty = d1.difficulty
+
+
+
 
     //Download PDF
     const [docName, setDocName] = useState('')
@@ -60,18 +88,48 @@ const DiveCardSix = (props) => {
         //info
         doc.setFont('Courier')
         doc.setFontSize(10)
-        doc.text(29, 16, name)
-        doc.text(33, 20.5, team)
-        doc.text(30.6, 24.8, coach)
-        doc.text(163, 16, meet)
-        doc.text(163, 20.5, date)
-        doc.text(165.5, 25, '1-Meter, 6 Dive')
+        doc.text(name, 29, 16)
+        doc.text(team, 33, 20.5)
+        doc.text(coach, 30.6, 24.8)
+        doc.text(meet, 163, 16)
+        doc.text(date, 163, 20.5)
+        doc.text('1-Meter, 6 Dive', 165.5, 25)
         //dives
         //dive one
-        doc.text(50, 50, d1.direction)
-        // doc.text(65, 50, d1.rotation)
-        // doc.text(80, 50, d1.position)
-        // doc.text(95, 50, d1.difficulty)
+        doc.text(d1.num + divePos[0], 26, 67)
+        doc.text(d1.direction + " " + d1.rotation, 41, 67,)
+        doc.text(position[0], 99, 67)
+        doc.text('' + volDifficulty, 115, 67)
+        //dive two
+        doc.text(d2.num + divePos[1], 26, 77)
+        doc.text(d2.direction + " " + d2.rotation, 41, 77,)
+        doc.text(position[1], 99, 77)
+        doc.text(d2.difficulty, 115, 77)
+        //dive three
+        doc.text(d3.num + divePos[2], 26, 86.5)
+        doc.text(d3.direction + " " + d3.rotation, 41, 86.5,)
+        doc.text(position[2], 99, 86.5)
+        doc.text(d3.difficulty, 115, 86.5)
+        //dive four
+        doc.text(d4.num + divePos[3], 26, 96)
+        doc.text(d4.direction + " " + d4.rotation, 41, 96,)
+        doc.text(position[3], 99, 96)
+        doc.text(d4.difficulty, 115, 96)
+        //dive five
+        doc.text(d5.num + divePos[4], 26, 106)
+        doc.text(d5.direction + " " + d5.rotation, 41, 106,)
+        doc.text(position[4], 99, 106)
+        doc.text(d5.difficulty, 115, 106)
+        //dive six
+        doc.text(d6.num + divePos[5], 26, 116)
+        doc.text(d6.direction + " " + d6.rotation, 41, 116,)
+        doc.text(position[5], 99, 116)
+        doc.text(d6.difficulty, 115, 116)
+        //Vol dd
+        doc.text("" + volDifficulty, 87, 132.7)
+        //Opt dd
+        doc.text("" + finalDD, 109, 132.7)
+
 
         doc.save(docName)
     }
